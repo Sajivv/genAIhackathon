803467de2e16f52cdf6c8eb503f7819747e2617b
Sajiv <51457044+Sajivv@users.noreply.github.com>
2024-05-22 09:34:42 -0400

    Initial commit

---
+++ components/avatar.js
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import { Image, Link as A } from 'rebass'
+
+export default ({ size = 50, light = false, ...props }) => (
+  <Link href="/">
+    <A sx={{ cursor: 'pointer', lineHeight: 0 }}>
+      <Image
+        {...props}
+        src="/static/HackHoundsLogo.png"
+        alt="HackHounds avatar"
+        width={size}
+        height={size}
+        sx={{
+          borderRadius: 'circle',
+          overflow: 'hidden',
+          bg: 'primary',
+          ...props.sx
+        }}
+      />
+    </A>
+  </Link>
+)
